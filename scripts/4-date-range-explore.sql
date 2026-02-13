0b400bdf257bd6550881b480aec45625eafe5f8b
Sourav Shinde <55348863+souravshinde12@users.noreply.github.com>
2026-02-13 13:47:35 +1100

    Add SQL script for date range exploration
    This SQL script determines the date range of orders and the age range of customers.

---
+++ scripts/4-date-range-explore.sql
@@ -0,0 +1,23 @@
+/*
+=================================
+Date Range 
+=================================
+Purpose:
+  Determine boundaries of key data points.
+  Understand range of historical data.
+=================================
+*/
+-- First and Last date of order
+SELECT 
+    MIN(order_date) AS first_order_date,
+    MAX(order_date) AS last_order_date,
+    DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
+FROM gold.fact_sales;
+
+-- Youngest and oldest customer based on birthdate
+SELECT
+    MIN(birthdate) AS oldest_birthdate,
+    DATEDIFF(YEAR, MIN(birthdate), GETDATE()) AS oldest_age,
+    MAX(birthdate) AS youngest_birthdate,
+    DATEDIFF(YEAR, MAX(birthdate), GETDATE()) AS youngest_age
+FROM gold.dim_customers;

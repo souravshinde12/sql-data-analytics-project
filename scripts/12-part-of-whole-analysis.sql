17c9be02cd3010c2744c12948f4c57c2b0cd6a35
Sourav Shinde <55348863+souravshinde12@users.noreply.github.com>
2026-02-13 14:15:45 +1100

    Add part-to-whole analysis SQL script
    This SQL script performs a part-to-whole analysis by comparing sales across product categories and calculating their contribution to overall sales.

---
+++ scripts/12-part-of-whole-analysis.sql
@@ -0,0 +1,26 @@
+/*
+======================================
+Part-to-Whole Analysis
+======================================
+Purpose:
+  Compare performance or metrics across dimensions or time periods.
+  Evaluate differences between categories.
+======================================
+*/
+-- Categories contribute the most to overall sales
+WITH category_sales AS (
+    SELECT
+        p.category,
+        SUM(f.sales_amount) AS total_sales
+    FROM gold.fact_sales f
+    LEFT JOIN gold.dim_products p
+        ON p.product_key = f.product_key
+    GROUP BY p.category
+)
+SELECT
+    category,
+    total_sales,
+    SUM(total_sales) OVER () AS overall_sales,
+    ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100, 2) AS percentage_of_total
+FROM category_sales
+ORDER BY total_sales DESC;
